c2831c6a926db469ee22d14b7c41ab3ce667006c
hajara bibi <40460495+Hajaraabibi@users.noreply.github.com>
2021-03-25 09:03:11 +0000

    Add files via upload

---
+++ sequenceandmeanandsum.R
@@ -0,0 +1,7 @@
+print("find sequence of numbers 20 to 50: ")
+print(seq(20,50))
+print("find mean of numbers 20 to 60")
+print(mean(20:60))
+print("find sum of numbers 51 to 91")
+print(sum(51:91))
+      
